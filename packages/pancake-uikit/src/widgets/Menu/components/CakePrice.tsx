117e5b60cabbb58433b3c08346e0450345fd8fcf
engineer325 <engineerlife325@gmail.com>
2021-11-20 03:14:02 +1000

    update price component

--- packages/pancake-uikit/src/widgets/Menu/components/CakePrice.tsx
+++ packages/pancake-uikit/src/widgets/Menu/components/CakePrice.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { PancakeRoundIcon } from "../../../components/Svg";
 import Text from "../../../components/Text/Text";
+import Image from "../../../components/Image/Image";
 import Skeleton from "../../../components/Skeleton/Skeleton";
 
 interface Props {
@@ -11,15 +12,19 @@ interface Props {
 const PriceLink = styled.a`
   display: flex;
   align-items: center;
-  svg {
+  img {
     transition: transform 0.3s;
   }
   :hover {
-    svg {
+    img {
       transform: scale(1.2);
     }
   }
 `;
+
+const StyledImage = styled.img`
+  margin-right: 8px;
+`
 
 const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
   return cakePriceUsd ? (
@@ -27,7 +32,8 @@ const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
       href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
       target="_blank"
     >
-      <PancakeRoundIcon width="24px" mr="8px" />
+      {/* <PancakeRoundIcon width="24px" mr="8px" /> */}
+      <StyledImage src="/logo.png" width={24} height={26} />
       <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
     </PriceLink>
   ) : (
